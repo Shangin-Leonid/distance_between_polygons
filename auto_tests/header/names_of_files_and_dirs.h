ad951cb936842293269771b80b12ce74e6bfa056
Leonid Shangin <leonid.shangin@math.msu.ru>
2023-05-24 16:15:57 +0300

    add header with file names

---
+++ auto_tests/header/names_of_files_and_dirs.h
@@ -0,0 +1,12 @@
+#ifndef NAMES_OF_FILES_AND_DIRS_H
+#define NAMES_OF_FILES_AND_DIRS_H
+
+
+#define NAME_OF_FILL_IN_PROGRAMM "fill_in_testing_output"
+#define NAME_OF_TEST_OUTPUT_FILE "./test_report/test_output.txt"
+#define NAME_OF_TEST_REPORT_FILE "./test_report/test_results.txt"
+#define TEST_CASES_DIR "./test_cases"
+#define CASE_PREFIX "case_"
+
+
+#endif /* NAMES_OF_FILES_AND_DIRS_H */
